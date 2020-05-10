2d27c8dd21157a7c3feffd521c9f81d0bf871090
hulk1110 <Nishant@FISGLOBAL.COM>
2020-05-10 10:37:07 +0530

    writing to a file

--- NIO-API-DEMO/src/com/nish/ReadFiles.java
+++ NIO-API-DEMO/src/com/nish/ReadFiles.java
@@ -2,27 +2,28 @@ package com.nish;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class ReadFiles {
-public static void main(String[] args) {
-	
-	Path path= Paths.get("files/sonnet-UTF8.txt");
-	boolean exists = Files.exists(path);
-	System.out.println("Exist-->"+exists);
-	
-	try(BufferedReader bufferedReader= Files.newBufferedReader(path);){
-		
-		String line = bufferedReader.readLine();
-		while (line!=null) {
-			System.out.println("line->"+line);
-			line = bufferedReader.readLine();
+	public static void main(String[] args) {
+
+		Path path = Paths.get("files/sonnet-ISO.txt");
+		boolean exists = Files.exists(path);
+		System.out.println("Exist-->" + exists);
+		// if special character is there use ISO_8859_1
+		try (BufferedReader bufferedReader = Files.newBufferedReader(path, StandardCharsets.ISO_8859_1);) {
+
+			String line = bufferedReader.readLine();
+			while (line != null) {
+				System.out.println("line->" + line);
+				line = bufferedReader.readLine();
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
 		}
-		
-	}catch (IOException e) {
-		e.printStackTrace();
 	}
-}
 }
---
+++ NIO-API-DEMO/src/com/nish/WriteFiles.java
@@ -0,0 +1,30 @@
+package com.nish;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class WriteFiles {
+	public static void main(String[] args) {
+
+		Path path = Paths.get("files/debug.txt");
+		boolean exists = Files.exists(path);
+		System.out.println("Exist-->" + exists);
+		// if special character is there use ISO_8859_1
+		try (BufferedWriter bufferedWriter = Files.newBufferedWriter(path);
+				BufferedWriter bufferedWriter2 = new BufferedWriter(bufferedWriter);
+				PrintWriter printWriter = new PrintWriter(bufferedWriter2);) {
+
+			bufferedWriter.write("writing in debug file.....");
+			printWriter.printf("\ni=%d\n", 12);
+		}
+
+		catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
